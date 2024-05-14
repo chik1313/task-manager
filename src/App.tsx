abf9da4ad34605ad5a0732606e5a2742bba0bd51
chik <ochkurov96@gmail.com>
2024-05-14 22:34:18 +0300

    add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
     let [todolists, setTodolists] = useState<TodolistsType[]>([
         {id: todolistId1, title: "What to learn", filter: "all"},
-        {id: todolistId2, title: "What to buy", filter: "all"},
+        {id: todolistId2, title: "What tsssso buy", filter: "all"},
     ])
 
     let [tasks, setTasks] = useState<TasksType>({
