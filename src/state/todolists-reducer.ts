be1136a590ac6561b877236705d7d7937cc8341f
chik <ochkurov96@gmail.com>
2024-05-22 22:15:51 +0300

    add tests

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,4 +1,5 @@
 import {TodolistsType} from "../App";
+import {v1} from "uuid";
 
 type ActionType = {
     type:string,
@@ -7,6 +8,12 @@ type ActionType = {
 
 export const todolistsReducer = (state:TodolistsType[] , action: ActionType) => {
     switch (action.type) {
+        case 'REMOVE-TODOLIST': {
+            return state.filter(tl => tl.id != action.payload.id)
+        }
+        case 'ADD-TODOLIST': {
+            return [...state ,  { id: v1(), title: action.payload.title, filter: 'all' }]
+        }
         default:
             throw new Error("I don't understand this action type")
     }
