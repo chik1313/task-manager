d65173e81020244390d239961241922d590a00e1
chik1313 <ochkurov96@gmail.com>
2024-08-17 19:59:59 +0200

    some refactor

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -15,7 +15,7 @@ type changeTasksStatusAcType = {
 }
 
 type changeTasksTitleAcType = {
-    type: "CHANGE-TASK-TITLE"
+    type: "CHANGE-TASK-TITыLE"
     taskId: string
     title: string
     todolistId: string
