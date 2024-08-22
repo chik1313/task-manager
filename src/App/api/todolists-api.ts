2bebd32023917a7a19619162294e2b0d4d4d04e8
chik1313 <ochkurov96@gmail.com>
2024-08-22 17:41:47 +0200

    some refactor

--- src/App/api/todolists-api.ts
+++ src/App/api/todolists-api.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+const settings = {
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'd45af3fe-3be5-4464-9868-74aefd92e6fe'
+    }
+}
+
 export const todolistsApi = {
     getTodolists() {
-        const promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists')
+        const promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
         return promise;
     }
 }
