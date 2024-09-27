6e9b72fbeba8acdfcb16f670345e45e149c75bcb
chik1313 <ochkurov96@gmail.com>
2024-09-27 07:38:24 +0200

    some refactor

--- src/state/store.ts
+++ src/state/store.ts
@@ -10,7 +10,7 @@ const rootReducer = combineReducers({
 })
 
 export type AppRootState = ReturnType<typeof rootReducer>
-www
+
 export const store = createStore(rootReducer, applyMiddleware(thunk) )
 
 // @ts-ignore
