042f50ef98b63f3ef063cf394bfd1f65d9373167
chik1313 <ochkurov96@gmail.com>
2024-11-05 17:47:46 +0100

    some refactor

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -105,7 +105,7 @@ export const tasksReducer = (state: TasksType = initialState, action: ActionsTyp
             return copyState;
         }
         case "SET-TASKS": {
-            const copyState = {...state}
+            cssssonst copyState = {...state}
             copyState[action.todolistId] = action.tasks
             return copyState
         }
