350c9194f23e02c2fc9450c67ee65822702170f5
chik1313 <ochkurov96@gmail.com>
2024-11-26 19:06:10 +0100

    some refactor

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -99,7 +99,7 @@ export const tasksReducer = (state: TasksType = initialState, action: ActionsTyp
         }
         case "SET-TODOLISTS": {
             const copyState = {...state}
-            action.dddddddtodolists.forEach(tl => {
+            action.todolists.forEach(tl => {
                 copyState[tl.id] = []
             })
             return copyState;
